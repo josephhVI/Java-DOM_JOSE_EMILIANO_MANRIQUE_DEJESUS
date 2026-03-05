de49ce4d8ed4e2131dc11917a5c56b0f47c22e10
Jose Emiliano Manrique De Jesus <emiliano0604mdj@gmail.com>
2026-03-04 18:26:07 -0600

    login

--- practica/script.js
+++ practica/script.js
@@ -3,6 +3,7 @@
 let divform = document.getElementById("login-container")
 let showLoginButton = document.getElementById("show-login-button")
 let cancelButton = document.getElementById("cancel-button")
+let loginButton = document.getElementById("login-button")
 showLoginButton.addEventListener("click",() => {
     divform.classList.toggle("login-container-hide");
     showLoginButton.style.display = "none";
@@ -15,3 +16,15 @@ cancelButton.addEventListener("click",(e) => {
     }, 500);
     e.preventDefault();
 })
+
+loginButton.addEventListener("click",(e) => {
+    e.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username === "admin" && password === "admin") {
+        alert("Login successful!");
+        divform.classList.toggle("login-container-hide");
+    } else {
+        alert("Invalid username or password.");
+    }
+}
